b67f637cc134589128fe6a32cacfaccde5b69e1f
Donald Ball <donald.ball@gmail.com>
2012-04-16 16:14:31 -0400

    Feature: destroy_without_callbacks

--- lib/skip_activerecord_callbacks/model_helpers.rb
+++ lib/skip_activerecord_callbacks/model_helpers.rb
@@ -4,14 +4,37 @@ module SkipActiveRecordCallbacks
       ModelDecorator.override_run_callbacks_to_skip_save_once(self)
       save
     end
+
+    def destroy_without_callbacks
+      ModelDecorator.override_run_callbacks_to_skip_destroy_once(self)
+      destroy
+    end
   end
 
+  # TODO: The cut and paste is sad
   class ModelDecorator
     def self.override_run_callbacks_to_skip_save_once(model)
       class << model
         alias :run_callbacks_orig :run_callbacks
         def run_callbacks(name, &block)
           if name == :save
+            class << self
+              undef :run_callbacks
+              alias :run_callbacks :run_callbacks_orig
+            end
+            yield
+          else
+            run_callbacks_orig(name, &block)
+          end
+        end
+      end
+    end
+
+    def self.override_run_callbacks_to_skip_destroy_once(model)
+      class << model
+        alias :run_callbacks_orig :run_callbacks
+        def run_callbacks(name, &block)
+          if name == :destroy
             class << self
               undef :run_callbacks
               alias :run_callbacks :run_callbacks_orig
--- test/skip_activerecord_callbacks_test.rb
+++ test/skip_activerecord_callbacks_test.rb
@@ -1,5 +1,10 @@
 require 'test_helper'
 
+class DestructionNotifier
+  def self.product_was_destroyed(product)
+  end
+end
+
 class Product < ActiveRecord::Base
   attr_accessible :name
 
@@ -9,6 +14,10 @@ class Product < ActiveRecord::Base
 
   after_save do |product|
     product.name = product.name.titleize
+  end
+
+  after_destroy do |product|
+    DestructionNotifier.product_was_destroyed(product)
   end
 end
 
@@ -42,6 +51,24 @@ describe SkipActiverecordCallbacks do
       product.update_without_callbacks
       product.save
       product.name.must_equal 'Bar'
+    end
+  end
+
+  describe "normal destroy" do
+    it "destroys the model and runs the callbacks" do
+      product.save
+      DestructionNotifier.expects(:product_was_destroyed).with(product)
+      product.destroy
+      assert product.destroyed?
+    end
+  end
+
+  describe "#destroy_without_callbacks" do
+    it "destroys a record without running the callbacks" do
+      product.save
+      DestructionNotifier.expects(:product_was_destroyed).never
+      product.destroy_without_callbacks
+      assert product.destroyed?
     end
   end
 end
